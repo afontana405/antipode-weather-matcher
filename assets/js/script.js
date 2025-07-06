810d7b19f67160d6307d271b8727d4770a60a1a1
afontana405 <alex13579@icloud.com>
2025-07-06 14:41:51 -0500

    basic api config set up, will need to be refactored later to better fit the project's needs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
 import { fetchWeatherApi } from 'openmeteo';
 
-const url = "https://api.open-meteo.com/v1/forecast";
-const responses = await fetchWeatherApi(url, params);
+async function getCurrentWeather(lat, lon) {
+    const params = {
+        latitude: lat,
+        longitude: lon,
+        current: ["temperature_2m", "pressure_msl"],
+    };
+
+    try {
+        const responses = await fetchWeatherApi("https://api.open-meteo.com/v1/forecast", params);
+        
+        const response = responses[0];
+        const current = response.current();
+        const temp = current.variables(0).value();
+        const pressure = current.variables(1).value();
+
+        console.log('temp:', temp, 'Pressure:', pressure)
+
+        return;
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
+}
+
+getCurrentWeather(40, -74)
